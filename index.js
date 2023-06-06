83b6b7d8531b16de8e90ad58a1d42bbd0c2c416a
Ed Stokes <edstokes96@gmail.com>
2023-06-06 16:39:10 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
+
+function reverseString(word) {
+  // create an array from the input string
+  const wordArray = word.split("");
+  // reverse the array
+  const reverseWordArray = wordArray.reverse();
+  // create string from the reversed array
+  const reversedWord = reverseWordArray.join("");
+  return reversedWord;
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
+
 }
 
 /* 
   Add your pseudocode here
+  reverse the input string
+
+  if the revesred string is the same as the input
+    return true
+  else
+    return false
 */
 
-/*
-  Add written explanation of your solution here
-*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
